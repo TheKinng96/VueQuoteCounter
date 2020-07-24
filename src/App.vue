2c5c6440223d2bfa76f4d402ec8998843030a211
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-24 20:27:00 +0900

    implemented quote adding methods

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-quote-card :quotes="quotes"></app-quote-card>
+  </div>
 </template>
 
 <script>
-export default {};
+import QuoteCard from "./components/QuoteCard.vue";
+
+export default {
+  data: function () {
+    return {
+      quotes: ["The dummy string of quote"],
+      maxQuote: 10,
+    };
+  },
+  components: {
+    appQuoteCard: QuoteCard,
+  },
+};
 </script>
 
 <style></style>
